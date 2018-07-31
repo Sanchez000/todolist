d2be644adfe1be7b6621d6a67178c900ffe35926
Sanchez000 <40805089+Sanchez000@users.noreply.github.com>
2018-07-31 16:09:33 +0300

    Update Users.php

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -45,6 +45,7 @@ public function new_user_registration() {
 $this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
 $this->form_validation->set_rules('email_value', 'Email', 'trim|required|xss_clean');
 $this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean');
+	
 if ($this->form_validation->run() == FALSE) {
 $this->load->view('registration_form');
 } else {
@@ -53,10 +54,15 @@ $data = array(
 'user_email' => $this->input->post('email_value'),
 'user_password' => $this->input->post('password')
 );
+	
+//Put new user to DB	
 $result = $this->login_database->registration_insert($data);
-if ($result == TRUE) {
+	
+//If new user
+	if ($result == TRUE) {
 $data['message_display'] = 'Registration Successfully !';
 $this->load->view('login_form', $data);
+//If user with that Username is already exist in DB
 } else {
 $data['message_display'] = 'Username already exist!';
 $this->load->view('registration_form', $data);
@@ -71,13 +77,12 @@ $this->form_validation->set_rules('username', 'Username', 'trim|required|xss_cle
 $this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean');
 
 if ($this->form_validation->run() == FALSE) {
-if(isset($this->session->userdata['logged_in'])){
-    //When user is logined
-redirect('./Main');
-//$this->load->view('admin_page');
-}else{
-$this->load->view('login_form');
-}
+			if(isset($this->session->userdata['logged_in'])){
+							//When user is logined
+			redirect('./Main');
+			}else{
+			$this->load->view('login_form');
+			}
 } else {
 $data = array(
 'username' => $this->input->post('username'),
@@ -97,7 +102,7 @@ if ($result != false) {
         // Add user data in session
         $this->session->set_userdata('logged_in', $session_data);
         //if valid data of user - RUN
-        //начало эксперимента
+        //Go
         $this->load->model('todolist_model');
         
          $allPrjcts = $this->todolist_model->get_all_projects();
@@ -139,9 +144,7 @@ if ($result != false) {
           }
           
         
-        //конец эксперимента
-        //redirect('./Main');
-        //$this->load->view('admin_page');
+        //END of GO
 }
 } else {
 $data = array(
